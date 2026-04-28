06ae8372d65b2ece1e77b1b7bd7fd517700746db
Jec <71040074+JecmE@users.noreply.github.com>
2026-04-28 17:22:56 -0600

    Versión final Fase 2

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mayarch-fpc5dvefa9cycne9.centralus-01.azurewebsites.net/api',
+  apiUrl: 'https://mayarch.azurewebsites.net/api',
 };
