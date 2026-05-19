d5011a01b06618798f91bc25f3683e60297e30ef
Jec <71040074+JecmE@users.noreply.github.com>
2026-05-19 16:40:16 -0600

    final

--- test-login.ps1
+++
@@ -1,7 +0,0 @@
-$body = @{
-    username = "mariagarcia"
-    password = "Test1234"
-} | ConvertTo-Json
-
-$response = Invoke-RestMethod -Uri "http://localhost:3000/api/auth/login" -Method POST -Body $body -ContentType "application/json"
-$response | ConvertTo-Json
